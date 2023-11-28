6e187eaec2b4279a2d1580f62e072dabdb1aa08b
kaikyoliveirasilva@gmail.com <Kaiky>
2023-11-28 20:00:51 -0300

    tst

--- (JS) ATIVIDADES/Caulculadora - IMC/script.js
+++ (JS) ATIVIDADES/Caulculadora - IMC/script.js
@@ -18,17 +18,20 @@ function calcularIMC() {
 }
 
 function classificarIMC(imc) {
-    if (imc < 18.5) {
+    if (imc <= 16.9) {
+        return 'Muito abaixo do peso';
+    } else if (imc >= 17 && imc <= 18.4) {
         return 'Abaixo do peso';
-    } else if (imc < 24.9) {
+    } else if (imc >= 18.5 && imc <= 24.9) {
         return 'Peso normal';
-    } else if (imc < 29.9) {
-        return 'Sobrepeso';
-    } else if (imc < 34.9) {
+    } else if (imc >= 25 && imc <= 29.9) {
+        return 'Acima do Peso';
+    } else if (imc >= 30 && imc <= 34.9) {
         return 'Obesidade grau 1';
-    } else if (imc < 39.9) {
+    } else if (imc >= 35 && imc <= 40) {
         return 'Obesidade grau 2';
     } else {
         return 'Obesidade grau 3';
     }
 }
+
