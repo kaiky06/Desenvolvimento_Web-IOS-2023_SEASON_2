c10468d1a15c517247f4c4bef7bb877344e71225
kaikyoliveirasilva@gmail.com <Kaiky>
2023-12-03 05:38:30 -0300

    exercicio-04

---
+++ REACT/exercicio/exercicio-4/src/Components/Conteudo.js
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import './Conteudo.css'; // Certifique-se de ter um arquivo CSS para estilos
+
+const Conteudo = () => {
+  const [contador, setContador] = useState('');
+  const [lido, setLido] = useState('');
+  const [feedback, setFeedback] = useState('');
+
+  const mensagens = {
+    good: 'Bom',
+    bad: 'Ruim',
+    terrible: 'Péssimo',
+  };
+
+  const handleLidoClick = () => {
+    setLido('excellent');
+    setFeedback('excellent-feedback');
+  };
+
+  const handleBomClick = () => {
+    setContador('good');
+    setFeedback('good-feedback');
+  };
+
+  const handleRuimClick = () => {
+    setContador('bad');
+    setFeedback('bad-feedback');
+  };
+
+  const handlePessimoClick = () => {
+    setContador('terrible');
+    setFeedback('terrible-feedback');
+  };
+
+  return (
+    <div>
+      <article>
+        <h2>IOS-Instituto da Oportunidade Social</h2>
+        <p>JÁ PODE AVALIAR</p>
+        <div>
+          <p className={feedback}>{lido && mensagens[lido]}{contador && mensagens[contador]}</p>
+          <button onClick={handleLidoClick}>
+            Ótimo
+          </button>
+          <button onClick={handleBomClick}>
+            Bom
+          </button>
+          <button onClick={handleRuimClick}>
+            Ruim
+          </button>
+          <button onClick={handlePessimoClick}>
+            Péssimo   
+          </button>
+        </div>
+      </article>
+    </div>
+  );
+};
+
+export default Conteudo;
---
+++ REACT/exercicio/exercicio-4/src/Components/Footer.js
@@ -0,0 +1,13 @@
+
+
+const estilo = { fontSize: '12px'  }
+
+const Footer = () => {
+  return (
+    <div>
+        <p style={ estilo }>&copy;KaikyOliveira</p>
+    </div>
+  )
+}
+
+export default Footer
